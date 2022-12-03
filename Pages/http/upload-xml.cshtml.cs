dea586ea272e73a2b98206354b061a2ca22f8126
Алексей <burdykin@live.ru>
2022-12-03 19:26:04 +0300

    сервисный аккаунт

--- Pages/http/upload-xml.cshtml.cs
+++ Pages/http/upload-xml.cshtml.cs
@@ -1,3 +1,4 @@
+using Amazon.S3;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,6 +6,15 @@ namespace YOS_CRUD.Pages.http
 {
     public class upload_xmlModel : PageModel
     {
+
+
+        //AmazonS3Config configsS3 = new AmazonS3Config
+        //{
+        //    ServiceURL = "https://s3.yandexcloud.net"
+        //};
+
+        //AmazonS3Client s3client = new AmazonS3Client(configsS3);
+
         public void OnGet()
         {
         }
