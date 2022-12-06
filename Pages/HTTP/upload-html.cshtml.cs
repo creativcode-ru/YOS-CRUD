99b7f530e35c1fbe627dfe81ed17a38238196d06
Алексей <burdykin@live.ru>
2022-12-06 16:50:55 +0300

    upload html

---
+++ Pages/HTTP/upload-html.cshtml.cs
@@ -0,0 +1,13 @@
+using Amazon.S3;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace YOS_CRUD.Pages.http
+{
+    public class upload_htmlModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
